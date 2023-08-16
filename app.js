dff3d2fca8927524f4c1e81e2d36d9388e64d197
Abdulbasid Haruna <lbaseed2010@gmail.com>
2023-08-16 01:17:58 +0100

    mongodb data storage done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
 
 const app = express();
 
@@ -25,7 +27,7 @@ app.use(
                 title: String!
                 description: String!
                 price: Float!
-                date: String
+                date: String!
             }
 
             type RootQuery {
@@ -43,24 +45,55 @@ app.use(
         `),
 		rootValue: {
 			events: () => {
-				return events;
+				return Event.find()
+					.then((events) => {
+						return events.map((event) => {
+							return { ...event._doc };
+						});
+					})
+					.catch((err) => {
+						console.log(err);
+						throw err;
+					});
 			},
 			createEvent: (args) => {
-				const event = {
-                    _id: Math.random().toString(),
-                    title: args.eventInput.title,
-                    description: args.eventInput.description,
-                    price: +args.eventInput.price,
-                    date: new Date().toISOString()
-                }
+				// const event = {
+				// 	_id: Math.random().toString(),
+				// 	title: args.eventInput.title,
+				// 	description: args.eventInput.description,
+				// 	price: +args.eventInput.price,
+				// 	date: new Date().toISOString(),
+				// };
 
-                events.push(event)
+				const event = new Event({
+					title: args.eventInput.title,
+					description: args.eventInput.description,
+					price: +args.eventInput.price,
+					date: new Date(args.eventInput.date),
+				});
 
-                return event;
+				return event
+					.save()
+					.then((result) => {
+						return { ...result._doc };
+					})
+					.catch((err) => {
+						console.log(err);
+						throw err;
+					});
 			},
 		},
 		graphiql: true,
 	})
 );
 
-app.listen(3000);
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.r3uddy1.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+	)
+	.then(() => {
+		app.listen(3000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
